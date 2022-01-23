730fcf46f5d760798233cdb633fe1c753a262c31
donkeedong <chase.steenbock@gmail.com>
2022-01-23 16:33:20 -0600

    made the menu responsive and cleaned up its code a little

--- util/getLayout.tsx
+++ util/getLayout.tsx
@@ -1,53 +1,87 @@
-import {
-  GithubOutlined,
-  HomeOutlined,
-  MehOutlined,
-  UserOutlined
-} from '@ant-design/icons';
+import { GithubOutlined, HomeTwoTone, MehTwoTone } from '@ant-design/icons';
 import { Menu } from 'antd';
 import Avatar from 'antd/lib/avatar/avatar';
 import Layout, { Content, Header } from 'antd/lib/layout/layout';
 import Sider from 'antd/lib/layout/Sider';
 import Text from 'antd/lib/typography/Text';
 import Link from 'next/link';
-import { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import style from '../styles/getLayout.module.css';
 
 export const getLayout = (page: ReactElement) => {
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
+  const [isCollapsed, setCollapsed] = useState(false);
+
+  useEffect(() => {
+    if (isTabletOrMobile) {
+      setCollapsed(true);
+    }
+  }, []);
+
   return (
-    <Layout>
+    <Layout style={{ minHeight: '100vh' }}>
       <Header>
-        <Avatar icon={<MehOutlined />} className={style.logo} />
+        <Avatar
+          icon={<MehTwoTone twoToneColor="#52c41a" />}
+          className={style.logo}
+        />
         <Text className={style.headername}>Chase Steenbock</Text>
       </Header>
       <Layout>
-        <Sider>
-          <Menu mode="vertical">
-            <Menu.Item key={'home'}>
-              <Link href="/">
-                <div>
-                  <HomeOutlined className={style.menuicon} />
-                  Home
-                </div>
-              </Link>
+        <Sider
+          collapsible
+          collapsed={isCollapsed}
+          onCollapse={() => setCollapsed(!isCollapsed)}
+        >
+          <Menu mode="inline" defaultSelectedKeys={['home']}>
+            <Menu.Item
+              key={'home'}
+              icon={
+                <HomeTwoTone
+                  style={{
+                    marginRight: isCollapsed ? 8 : undefined
+                  }}
+                />
+              }
+              style={{ paddingLeft: isCollapsed ? 32 : undefined }}
+            >
+              <Link href="/">Home</Link>
             </Menu.Item>
-            <Menu.Item key={'about'}>
-              <Link href="/about">
-                <div>
-                  <UserOutlined className={style.menuicon} />
-                  About Me
-                </div>
-              </Link>
+            <Menu.Item
+              key={'about'}
+              icon={
+                <MehTwoTone
+                  twoToneColor="#52c41a"
+                  style={{
+                    marginRight: isCollapsed ? 8 : undefined
+                  }}
+                />
+              }
+              style={{ paddingLeft: isCollapsed ? 32 : undefined }}
+            >
+              <Link href="/about">About Me</Link>
             </Menu.Item>
-            <Menu.Item key={'github'}>
+            <Menu.Item
+              key={'github'}
+              icon={
+                <GithubOutlined
+                  style={{
+                    marginRight: isCollapsed ? 8 : undefined
+                  }}
+                />
+              }
+              style={{ paddingLeft: isCollapsed ? 32 : undefined }}
+            >
               <a href="https://github.com/donkeedong" target={'_blank'}>
-                <GithubOutlined className={style.menuicon} />
                 My GitHub
               </a>
             </Menu.Item>
           </Menu>
         </Sider>
-        <Content className={style.internalcontent}>{page}</Content>
+        <Layout className={style.internalcontent}>
+          <Content>{page}</Content>
+        </Layout>
       </Layout>
     </Layout>
   );
